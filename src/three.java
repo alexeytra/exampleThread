19a42af8c75ea95e9351e2a49c36886f9f17f311
alex <alekceytr@mail.ru>
2018-12-03 22:02:12 +0800

    Применение синхрон.потоков

---
+++ src/three.java
@@ -0,0 +1,47 @@
+//Применение синхронизированных методов
+public class three {
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Добро пожаловать");
+        Caller ob2 = new Caller(target, "в синхронизированный");
+        Caller ob3 = new Caller(target, "мир!");
+
+        try {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Прервано");
+        }
+    }
+}
+
+class Callme {
+    synchronized void call(String msg){
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Прервано");
+        }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme target, String s){
+        this.target = target;
+        this.msg = s;
+        this.t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        target.call(msg);
+    }
+}
